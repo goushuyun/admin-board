ae9c0e83cd251310e9e9e3f6727a049575c79f82
fengzhongsen <fengzhongsen@gmail.com>
2017-02-19 13:17:18 +0800

    Added some events.

--- src/pages/board/orders/orders_check.vue
+++ src/pages/board/orders/orders_check.vue
@@ -1,5 +1,4 @@
 <style lang="scss" scoped>
-
 div.content {
     .el-form-item {
         margin-bottom: 12px;
@@ -46,11 +45,9 @@ div.content {
         text-align: center;
     }
 }
-
 table.order_items:hover {
     background-color: #EFF2F7;
 }
-
 </style>
 
 <template lang="html">
@@ -125,60 +122,60 @@ table.order_items:hover {
 
         <el-table-column label="订单详情" width="400" class="items">
             <template scope="scope">
-                <div class="order_items">
-                    <p class="order_item_info" v-for="item in scope.row.items">
-                        <span class="book_title">
+<div class="order_items">
+    <p class="order_item_info" v-for="item in scope.row.items">
+        <span class="book_title">
                             {{ '《' + item.book_title + '》'}}
                             <span v-if="item.type==1" class="new">新书</span>
-                        <span v-if="item.type==2" class="old">二手书</span>
-                        </span>
-                        <span class="book_isbn">{{ item.isbn }}</span>
-                        <span class="book_number">{{ 'x  ' + item.number }}</span>
-                    </p>
-                </div>
-            </template>
+        <span v-if="item.type==2" class="old">二手书</span>
+        </span>
+        <span class="book_isbn">{{ item.isbn }}</span>
+        <span class="book_number">{{ 'x  ' + item.number }}</span>
+    </p>
+</div>
+</template>
         </el-table-column>
 
         <!-- <el-table-column label="退款">
             <template scope="scope">
-                <div v-if="scope.row.refund">
-                    <p class="center" style="color: #FF4949">待退款</p>
-                    <p class="center">
-                        <el-button type="text" size="small">查看详情</el-button>
-                    </p>
-                    <p class="center">
-                        <el-button type="danger" size="mini">退款</el-button>
-                    </p>
-                </div>
-            </template>
+<div v-if="scope.row.refund">
+    <p class="center" style="color: #FF4949">待退款</p>
+    <p class="center">
+        <el-button type="text" size="small">查看详情</el-button>
+    </p>
+    <p class="center">
+        <el-button type="danger" size="mini">退款</el-button>
+    </p>
+</div>
+</template>
         </el-table-column> -->
 
         <el-table-column label="收货人信息" min-width="140">
             <template scope="scope">
-                <div>
-                    <p class="center">{{scope.row.address_info.name}}</p>
-                    <p class="center">{{scope.row.address_info.tel}}</p>
-                </div>
-            </template>
+<div>
+    <p class="center">{{scope.row.address_info.name}}</p>
+    <p class="center">{{scope.row.address_info.tel}}</p>
+</div>
+</template>
         </el-table-column>
 
         <el-table-column label="订单状态">
             <template scope="scope">
-                <div>
-                    <p class="center" v-if="scope.row.order_status == 2">待发货</p>
-                    <p class="center" v-if="scope.row.order_status == 3">已发货</p>
-                    <p class="center" v-if="scope.row.order_status == 4">已完成</p>
-                    <p class="center" v-if="scope.row.order_status == 5">已关闭</p>
+<div>
+    <p class="center" v-if="scope.row.order_status == 2">待发货</p>
+    <p class="center" v-if="scope.row.order_status == 3">已发货</p>
+    <p class="center" v-if="scope.row.order_status == 4">已完成</p>
+    <p class="center" v-if="scope.row.order_status == 5">已关闭</p>
 
-                    <p class="center">
-                        <el-button type="text" size="small" @click="viewDetial(scope.row.order_id)">查看详情</el-button>
-                    </p>
-                    <p class="center">
-                        <el-button type="primary" size="mini" v-if="scope.row.order_status == 2" @click="sendOrder(scope.row.order_id, scope.$index)">发货</el-button>
-                        <el-button type="primary" size="mini" v-if="scope.row.order_status == 3" @click="completeOrder(scope.row.order_id, scope.$index)">交易完成</button>
-                    </p>
-                </div>
-            </template>
+    <p class="center">
+        <el-button type="text" size="small" @click="viewDetial(scope.row.order_id)">查看详情</el-button>
+    </p>
+    <p class="center">
+        <el-button type="primary" size="mini" v-if="scope.row.order_status == 2" @click="sendOrder(scope.row.order_id, scope.$index)">发货</el-button>
+        <el-button type="primary" size="mini" v-if="scope.row.order_status == 3" @click="completeOrder(scope.row.order_id, scope.$index)">交易完成</button>
+    </p>
+</div>
+</template>
         </el-table-column>
 
         <el-table-column prop="total_price" label="实收款">
@@ -199,175 +196,203 @@ import enumList from '../../../scripts/enum'
 import axios from "../../../scripts/http"
 export default {
     mounted() {
+        this.getData()
+    },
+    data() {
+        return {
+            // 筛选条件
+            searchType: '',
+            searchValue: '',
+            order_time: '',
+            order_status: '2',
+
+            page: 1,
+            size: 10,
+            total: 0,
+            orders: [],
+
+            selected_orders: [],
+
+            loading: false,
+
+            pickerOptions2: {
+                shortcuts: [{
+                    text: '最近一周',
+                    onClick(picker) {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                        picker.$emit('pick', [start, end]);
+                    }
+                }, {
+                    text: '最近一个月',
+                    onClick(picker) {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                        picker.$emit('pick', [start, end]);
+                    }
+                }, {
+                    text: '最近三个月',
+                    onClick(picker) {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                        picker.$emit('pick', [start, end]);
+                    }
+                }]
+            },
+        }
+    },
+    methods: {
+        viewDetial(order_id) {
+            this.$router.push({
+                name: 'order_info',
+                params: {
+                    order_id
+                }
+            })
+        },
+
+        completeOrder(id, index) {
+            this.loading = true
+
+            axios.post('/v1/orders/complete_orders', {
+                order_ids: [id]
+            }).then(resp => {
+                if (resp.data.code == '00000') {
+                    this.$message({
+                        type: 'success',
+                        message: '订单' + id + '交易成功'
+                    })
+                    this.loading = false
+                }
+            })
+
+        },
+        sendSelectedOrders() {
+            this.loading = true
+            var order_ids = this.selected_orders.map(el => {
+                return el.order_id
+            })
+            if (order_ids.length < 1) {
+                this.$message({
+                    message: '您未选中任何订单!',
+                    type: 'warning'
+                })
+                this.loading = false
+                return
+            } else {
+                this.$confirm('您将批量发货, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    console.log(order_ids)
+                    axios.post('/v1/orders/send_orders', {
+                        order_ids
+                    }).then(resp => {
+                        if (resp.data.code == '00000') {
+                            this.$message({
+                                message: '订单批量发货成功',
+                                type: 'success'
+                            })
+                            this.getData()
+                            this.loading = false
+                        }
+                    })
+                }).catch(() => {
+                    this.loading = false
+                    this.$message({
+                        type: 'info',
+                        message: '已取消批量发货!'
+                    });
+                });
+
+            }
+        },
+
+
+        handleSizeChange(size) {
+            this.size = size
             this.getData()
         },
-        data() {
-            return {
-                // 筛选条件
-                searchType: '',
-                searchValue: '',
-                order_time: '',
-                order_status: '2',
-
-                page: 1,
-                size: 10,
-                total: 0,
-                orders: [],
-
-                selected_orders: [],
-
-                loading: false,
-
-                pickerOptions2: {
-                    shortcuts: [{
-                        text: '最近一周',
-                        onClick(picker) {
-                            const end = new Date();
-                            const start = new Date();
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-                            picker.$emit('pick', [start, end]);
-                        }
-                    }, {
-                        text: '最近一个月',
-                        onClick(picker) {
-                            const end = new Date();
-                            const start = new Date();
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-                            picker.$emit('pick', [start, end]);
-                        }
-                    }, {
-                        text: '最近三个月',
-                        onClick(picker) {
-                            const end = new Date();
-                            const start = new Date();
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-                            picker.$emit('pick', [start, end]);
-                        }
-                    }]
-                },
-            }
+        handleCurrentChange(page) {
+            this.page = page
+            this.getData()
         },
-        methods: {
-            viewDetial(order_id){
-                this.$router.push({name: 'order_info', params: {order_id}})
-            },
+        sendOrder(order_id, index) {
+            this.loading = true
 
-            completeOrder(id, index){
-                this.loading = true
-
-                axios.post('/v1/orders/complete_orders', {order_ids: [id]}).then(resp=>{
-                    if(resp.data.code == '00000'){
-                        this.$message({
-                            type:'success',
-                            message: '订单' + id + '交易成功'
-                        })
-                        this.loading = false
-                    }
-                })
-
-            },
-            sendSelectedOrders(){
-                this.loading = true
-                var order_ids = this.selected_orders.map(el => {
-                    return el.order_id
-                })
-                console.log(order_ids)
-                axios.post('/v1/orders/send_orders', {order_ids}).then(resp => {
-
-                    if(resp.data.code == '00000'){
-                        this.$message({
-                            message: '订单批量发货成功',
-                            type: 'success'
-                        })
-                        this.getData()
-                        this.loading = false
-                    }
-
-                })
-            },
-
-
-            handleSizeChange(size){
-                this.size = size
-                this.getData()
-            },
-            handleCurrentChange(page){
-                this.page = page
-                this.getData()
-            },
-            sendOrder(order_id, index){
-                this.loading = true
-
-                var order_ids = []
-                order_ids.push(order_id)
-                let data = {
-                    order_ids
-                }
-                axios.post('/v1/orders/send_orders', data).then(resp => {
-                    if(resp.data.code == '00000') {
-                        //发货成功
-                        this.orders.splice(index, 1)
-                        this.$message({
-                            message: '订单 '+ order_id +' 已发货',
-                            type: 'success'
-                        })
-                        this.loading = false
-                    }
-                })
-            },
-            search(){
-                this.getData()
-            },
-            changeSearchType() {
-                console.log(this.searchType)
-            },
-            handleSelectionChange(val) {
-
-                this.selected_orders = val
-
-                console.log(val)
-
-            },
-            getData() {
-                this.loading = true
-
-                let data = {
-                    page: this.page,
-                    size: this.size
-                }
-                data[this.searchType] = this.searchValue
-                data.order_at_start = moment(this.order_time[0], "YYYY-MM-DD").unix()
-                data.order_at_end = moment(this.order_time[1], "YYYY-MM-DD").unix()
-                data.order_status = parseInt(this.order_status)
-
-                axios.post('/v1/orders/listAllOrders', data).then(resp => {
-                    if (resp.data.code == '00000') {
-
-                        this.total = resp.data.total
-
-                        this.orders = resp.data.data.map(el => {
-                            el.order_at = moment.unix(el.order_at).format('YYYY-MM-DD HH:mm:ss')
-
-                            el.order_at_date = el.order_at.split(" ")[0]
-                            el.order_at_time = el.order_at.split(" ")[1]
-
-                            //设置订单状态
-                            el.order_status_name = enumList.order_status[el.order_status-1]
-
-                            if (el.pay_at != 0) {
-                                el.pay_at = moment.unix(el.pay_at).format('YYYY-MM-DD HH:mm:ss')
-                            }
-                            return el
-                        })
-
-                        this.loading = false
-                    }
-
-                    console.log(resp.data)
-                })
+            var order_ids = []
+            order_ids.push(order_id)
+            let data = {
+                order_ids
             }
+            axios.post('/v1/orders/send_orders', data).then(resp => {
+                if (resp.data.code == '00000') {
+                    //发货成功
+                    this.orders.splice(index, 1)
+                    this.$message({
+                        message: '订单 ' + order_id + ' 已发货',
+                        type: 'success'
+                    })
+                    this.loading = false
+                }
+            })
+        },
+        search() {
+            this.getData()
+        },
+        changeSearchType() {
+            console.log(this.searchType)
+        },
+        handleSelectionChange(val) {
+
+            this.selected_orders = val
+
+            console.log(val)
+
+        },
+        getData() {
+            this.loading = true
+
+            let data = {
+                page: this.page,
+                size: this.size
+            }
+            data[this.searchType] = this.searchValue
+            data.order_at_start = moment(this.order_time[0], "YYYY-MM-DD").unix()
+            data.order_at_end = moment(this.order_time[1], "YYYY-MM-DD").unix()
+            data.order_status = parseInt(this.order_status)
+
+            axios.post('/v1/orders/listAllOrders', data).then(resp => {
+                if (resp.data.code == '00000') {
+
+                    this.total = resp.data.total
+
+                    this.orders = resp.data.data.map(el => {
+                        el.order_at = moment.unix(el.order_at).format('YYYY-MM-DD HH:mm:ss')
+
+                        el.order_at_date = el.order_at.split(" ")[0]
+                        el.order_at_time = el.order_at.split(" ")[1]
+
+                        //设置订单状态
+                        el.order_status_name = enumList.order_status[el.order_status - 1]
+
+                        if (el.pay_at != 0) {
+                            el.pay_at = moment.unix(el.pay_at).format('YYYY-MM-DD HH:mm:ss')
+                        }
+                        return el
+                    })
+
+                    this.loading = false
+                }
+
+                console.log(resp.data)
+            })
         }
+    }
 
 }
-
 </script>
