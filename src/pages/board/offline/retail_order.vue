bb305348bc50399981909028feab640a9ab5e040
unknown <>
2017-02-28 13:25:33 +0800

    Projected retail_order.

--- src/pages/board/offline/retail_order.vue
+++ src/pages/board/offline/retail_order.vue
@@ -27,10 +27,10 @@ div.content {
             }
         }
         .book_title {
-            flex: 4;
+            flex: 8;
         }
         .book_isbn {
-            flex: 3;
+            flex: 4;
         }
         .book_price {
             flex: 2;
@@ -57,7 +57,7 @@ table.order_items:hover {
     <div class="search_item">
         <el-form class="searchPart" :inline="true">
             <el-form-item>
-                <el-date-picker v-model="order_time" size="small" type="daterange" placeholder="下单时间" :picker-options="pickerOptions2"></el-date-picker>
+              <el-date-picker v-model="order_time" type="datetimerange" size="small" :picker-options="pickerOptions2" placeholder="选择时间范围" align="right"></el-date-picker>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="search" icon="search" size="small">筛选</el-button>
@@ -65,20 +65,10 @@ table.order_items:hover {
         </el-form>
     </div>
 
-    <el-table :data="orders" border style="width: 100%" @selection-change="handleSelectionChange" stripe v-loading="loading" element-loading-text="拼命加载中">
-        <el-table-column type="selection" width="55">
-        </el-table-column>
-        <el-table-column label="订单号" width="160" prop="order_id">
-
-        </el-table-column>
-        <el-table-column label="下单时间" width="120">
-            <template scope="scope">
-                <p>{{scope.row.order_at_date}}</p>
-                <p>{{scope.row.order_at_time}}</p>
-            </template>
-        </el-table-column>
-
-        <el-table-column label="订单详情" width="600" class="items">
+    <el-table :data="orders" border style="width: 100%" stripe v-loading="loading" element-loading-text="拼命加载中">
+        <el-table-column label="订单号" width="190" prop="order_id"></el-table-column>
+        <el-table-column label="下单时间" width="180" prop="order_at"></el-table-column>
+        <el-table-column label="订单详情" width="580" class="items">
             <template scope="scope">
                 <div class="order_items">
                     <p class="order_item_info" v-for="item in scope.row.items">
@@ -87,14 +77,16 @@ table.order_items:hover {
                             <span v-if="item.type==2" class="old">二手书</span>
                         </span>
                         <span class="book_isbn">{{ item.isbn }}</span>
-                        <span class="book_price">{{ '￥18.00' }}</span>
+                        <span class="book_price">{{ '￥' + item.book_price }}</span>
                         <span class="book_number">{{ 'x' + item.number }}</span>
                     </p>
                 </div>
             </template>
         </el-table-column>
-        <el-table-column prop="total_price" label="实收款">
-
+        <el-table-column label="实收款">
+          <template scope="scope">
+              <p>{{'￥'+scope.row.total_price}}</p>
+          </template>
         </el-table-column>
     </el-table>
 
@@ -107,7 +99,6 @@ table.order_items:hover {
 </template>
 
 <script>
-import enumList from '../../../scripts/enum'
 import axios from "../../../scripts/http"
 export default {
     mounted() {
@@ -116,20 +107,12 @@ export default {
     data() {
         return {
             // 筛选条件
-            searchType: '',
-            searchValue: '',
             order_time: '',
-            order_status: '2',
-
             page: 1,
             size: 10,
             total: 0,
             orders: [],
-
-            selected_orders: [],
-
             loading: false,
-
             pickerOptions2: {
                 shortcuts: [{
                     text: '最近一周',
@@ -156,7 +139,7 @@ export default {
                         picker.$emit('pick', [start, end]);
                     }
                 }]
-            },
+            }
         }
     },
     methods: {
@@ -172,51 +155,33 @@ export default {
             this.getData()
         },
         handleSelectionChange(val) {
-
             this.selected_orders = val
-
             console.log(val)
-
         },
         getData() {
             this.loading = true
-
             let data = {
                 page: this.page,
-                size: this.size
+                size: this.size,
+                order_at_start: this.order_time? moment(this.order_time[0], "YYYY-MM-DD HH:mm:ss").unix() : 0,
+                order_at_end: this.order_time? moment(this.order_time[1], "YYYY-MM-DD HH:mm:ss").unix() : 0
             }
-            data[this.searchType] = this.searchValue
-            data.order_at_start = moment(this.order_time[0], "YYYY-MM-DD").unix()
-            data.order_at_end = moment(this.order_time[1], "YYYY-MM-DD").unix()
-            data.order_status = parseInt(this.order_status)
-
-            axios.post('/v1/orders/listAllOrders', data).then(resp => {
+            axios.post('/v1/orders/get_offline_orders', data).then(resp => {
                 if (resp.data.code == '00000') {
-
                     this.total = resp.data.total
-
                     this.orders = resp.data.data.map(el => {
                         el.order_at = moment.unix(el.order_at).format('YYYY-MM-DD HH:mm:ss')
-
-                        el.order_at_date = el.order_at.split(" ")[0]
-                        el.order_at_time = el.order_at.split(" ")[1]
-
-                        //设置订单状态
-                        el.order_status_name = enumList.order_status[el.order_status - 1]
-
-                        if (el.pay_at != 0) {
-                            el.pay_at = moment.unix(el.pay_at).format('YYYY-MM-DD HH:mm:ss')
-                        }
+                        el.total_price = (el.total_price/100).toFixed(2)
+                        el.items = el.items.map(it => {
+                            it.book_price = (it.book_price/100).toFixed(2)
+                            return it
+                        })
                         return el
                     })
-
                     this.loading = false
                 }
-
-                console.log(resp.data)
             })
         }
     }
-
 }
 </script>
