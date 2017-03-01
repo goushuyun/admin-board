2f81f25dfbe484f1f0f50ccf0ec0095c66664e4a
unknown <>
2017-03-02 00:32:02 +0800

    Profeted excel_upload page.

--- src/pages/board/addBooks/excel_upload.vue
+++ src/pages/board/addBooks/excel_upload.vue
@@ -1,26 +1,116 @@
 <style lang="scss" scoped>
+.box-card {
+    width: 600px;
+    margin-left: 15px;
+    .excel_upload {
+        margin: 20px 0 20px 80px;
+    }
+    .error {
+        margin: 40px 0;
+    }
+    .prompt {
+        margin-top: 20px;
+        color: #8391A5;
+        p {
+            font-size: 12px;
+        }
+    }
+}
 
-.excel_upload {}
+div p {
+    // height: 40px;
+    line-height: 40px;
+    margin-left: 10px;
+    font-size: 14px;
+    span {
+        font-weight: bold;
+    }
+    a {
+        margin: 0 10px;
+    }
+}
 
+.dialog-title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    color: red;
+    img {
+        width: 40px;
+        height: 40px;
+        margin-right: 5px;
+    }
+}
+.warning_pic{
+    width: auto;
+    margin: 10px;
+    img {
+      width: 100%;
+      border: 1px solid;
+    }
+}
 </style>
 
 <template lang="html">
+  <div>
+    <el-card class="box-card">
+      <div class="example">
+          <p>如果您还没有下载样例文件，请点击<a href="https://www.baidu.com">下载样例文件</a>，并编辑好商品信息后上传。</p>
+      </div>
+      <div class="excel_upload">
+          <el-upload name="excel" drag action="/v1/file/upload_books_excel" :show-file-list="true" :before-upload="beforeUpload">
+              <i class="el-icon-upload"></i>
+              <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+              <div class="el-upload__tip" slot="tip">只能上传xlsx文件，且不超过500kb</div>
+          </el-upload>
+      </div>
+      <div class="error">
+        <p><span>文件有误：</span><a href="https://www.baidu.com">xxxxxxx.xls</a>请点击下载此文件，按照要求修改后重新上传。</p>
+      </div>
 
-<div>
-    <el-row type="flex" justify="center">
-        <el-col :span="12">
-            <el-upload name="excel" class="excel_upload" drag action="/v1/file/upload_books_excel">
-                <el-button icon="upload" size="small" type="primary">点击上传</el-button>
-                <div slot="tip" class="el-upload__tip">只能上传xlsx文件，且不超过500kb</div>
-            </el-upload>
-        </el-col>
-    </el-row>
-</div>
+      <div class="prompt">
+         <p>上传须知：</p>
+         <p>出版社：请使用完整的出版社名称</p>
+         <p>类别：1.教材 2.教辅 3.外语 4.阅读 5.考证</p>
+         <p>类型：1.新书 2.旧书</p>
+         <p>仓库名、货架名：请使用完整的仓库名、货架名</p>
+      </div>
+    </el-card>
 
+    <el-dialog :show-close="false" :close-on-press-escape="false" :close-on-click-modal="false" v-model="dialogTableVisible">
+      <div slot="title" class="dialog-title">
+          <img src="src/images/warning.png"/><span>警告</span>
+      </div>
+      <div>
+          <p>· 请务必确保上传内容正确，否则会造成无法挽回的损失！</p>
+          <p>· 折扣示例：0.7表示7折 ，请保持折扣值在0~1之间！</p>
+          <p>· 请确保书名、出版社、作者等信息下方的信息是对应的。避免出现将书名填写在出版社下方的情况！</p>
+          <p>· 请使用完整的出版社名称以及仓库、货架名！</p>
+          <p>· 数据一旦上传将无法撤回，造成的一切后果请自行承担！</p>
+      </div>
+      <div class="warning_pic">
+          <img src="src/images/upload_warning.jpg">
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogTableVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogTableVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-
-export default {}
-
+export default {
+    data() {
+        return {
+            dialogTableVisible: false
+        }
+    },
+    methods: {
+        beforeUpload(file) {
+            this.dialogTableVisible = true
+        }
+    }
+}
 </script>
